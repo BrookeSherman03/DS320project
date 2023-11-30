6d584f9a6d9d864ebadacd9420edca82dcdc4e40
BrookeSherman03 <112006636+BrookeSherman03@users.noreply.github.com>
2023-11-29 20:48:55 -0500

    SQL Code

--- DS320queries.sql
+++ DS320queries.sql
@@ -1,20 +1,20 @@
 CREATE TABLE passingRating AS
-SELECT Player, Tm, Pos, P.Age, CmpPercent, YperG, QBR, R.Rating
-FROM passingdata AS P, roster AS R
-WHERE P.Player = R.ï»¿Player
+SELECT P.Player, Tm, Pos, P.Age, CmpPercent, YperG, QBR, R.Rating
+FROM Passing AS P, Roster AS R
+WHERE P.Player = R.Player
 AND P.Pos = R.Position
 ORDER BY R.Rating DESC;
 
 CREATE TABLE receivingRating AS
-SELECT Player, Tm, Pos, E.Age, CtchPercent, YperG, RperG, R.Rating
-FROM receivingdata AS E, roster AS R
-WHERE E.Player = R.ï»¿Player
+SELECT E.Player, Tm, Pos, E.Age, CtchPercent, YperG, RperG, R.Rating
+FROM Receivingdata AS E, Roster AS R
+WHERE E.Player = R.Player
 AND E.Pos = R.Position
 ORDER BY R.Rating DESC;
 
 CREATE TABLE rushingRating AS
-SELECT Player, Tm, Pos, U.Age, SuccPercent, YperG, YperA, R.Rating
-FROM rushingdata AS U, roster AS R
-WHERE U.Player = R.ï»¿Player
+SELECT U.Player, Tm, Pos, U.Age, SuccPercent, YperG, YperA, R.Rating
+FROM Rushing AS U, Roster AS R
+WHERE U.Player = R.Player
 AND U.Pos = R.Position
 ORDER BY R.Rating DESC;
